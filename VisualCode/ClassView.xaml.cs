ec472c611c84d4f5ddfeeb2bec3595d22f6711ca
tekavec <tomaz.tekavec@biromt.com>
2016-11-20 12:31:37 +0000

    Few code cleanups

--- VisualCode/ClassView.xaml.cs
+++ VisualCode/ClassView.xaml.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VisualCode
 {
-    public partial class ClassView : UserControl
+    public partial class ClassView
     {
         public ClassView()
         {
@@ -25,33 +14,26 @@ namespace VisualCode
         public ClassView(ClassView classView)
         {
             InitializeComponent();
-            this.ClassViewUi.Height = classView.ClassViewUi.Height;
-            this.ClassViewUi.Width = classView.ClassViewUi.Width;
-            this.ClassViewUi.Fill = classView.ClassViewUi.Fill;
+            ClassViewUi.Height = classView.ClassViewUi.Height;
+            ClassViewUi.Width = classView.ClassViewUi.Width;
+            ClassViewUi.Fill = classView.ClassViewUi.Fill;
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
         {
             base.OnMouseMove(e);
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                // Package the data.
-                DataObject data = new DataObject();
-                data.SetData(DataFormats.StringFormat, ClassViewUi.Fill.ToString());
-                data.SetData("Double", ClassViewUi.Width);
-                data.SetData("Double", ClassViewUi.Height);
-                data.SetData("Object", this);
-
-                // Inititate the drag-and-drop operation.
-                DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
-            }
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+            var data = new DataObject();
+            data.SetData(DataFormats.StringFormat, ClassViewUi.Fill.ToString());
+            data.SetData("Double", ClassViewUi.Width);
+            data.SetData("Double", ClassViewUi.Height);
+            data.SetData("Object", this);
+            DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
         }
 
         protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
         {
             base.OnGiveFeedback(e);
-            // These Effects values are set in the drop target's
-            // DragOver event handler.
             if (e.Effects.HasFlag(DragDropEffects.Copy))
             {
                 Mouse.SetCursor(Cursors.Cross);
@@ -70,31 +52,31 @@ namespace VisualCode
         protected override void OnDrop(DragEventArgs e)
         {
             base.OnDrop(e);
-
-            // If the DataObject contains string data, extract it.
             if (e.Data.GetDataPresent(DataFormats.StringFormat))
             {
-                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
-
-                // If the string can be converted into a Brush, 
-                // convert it and apply it to the ellipse.
-                BrushConverter converter = new BrushConverter();
-                if (converter.IsValid(dataString))
+                var dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+                var converter = new BrushConverter();
+                if (dataString != null && converter.IsValid(dataString))
                 {
-                    Brush newFill = (Brush)converter.ConvertFromString(dataString);
+                    var newFill = (Brush)converter.ConvertFromString(dataString);
                     ClassViewUi.Fill = newFill;
+                    e.Effects = e.KeyStates.HasFlag(DragDropKeyStates.ControlKey) ? DragDropEffects.Copy : DragDropEffects.Move;
+                }
+            }
+            e.Handled = true;
+        }
 
-                    // Set Effects to notify the drag source what effect
-                    // the drag-and-drop operation had.
-                    // (Copy if CTRL is pressed; otherwise, move.)
-                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
-                    {
-                        e.Effects = DragDropEffects.Copy;
-                    }
-                    else
-                    {
-                        e.Effects = DragDropEffects.Move;
-                    }
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            e.Effects = DragDropEffects.None;
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                var dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+                var converter = new BrushConverter();
+                if (dataString != null && converter.IsValid(dataString))
+                {
+                    e.Effects = e.KeyStates.HasFlag(DragDropKeyStates.ControlKey) ? DragDropEffects.Copy : DragDropEffects.Move;
                 }
             }
             e.Handled = true;
